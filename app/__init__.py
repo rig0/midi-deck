ef5f1abe023d84b9f9a3180a1fe6a915c144419a
Rigo <rigo-dev@pm.me>
2025-12-23 15:08:20 -0500

    sign

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,4 @@ Provides modular architecture for MIDI control, audio management,
 database persistence, and web interface.
 """
 
-__version__ = "1.1.0"
-__author__ = "MIDI Deck Development Team"
+__author__ = "Rig0"
